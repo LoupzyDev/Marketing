689955520ab861ecbbaa96c7752f04126abc660c
Dany Carrillo <danycarrito27@outlook.com>
2024-04-29 21:02:00 -0600

    Union de prototipo

--- Assets/Dany/Assets/Scripts/CoinCounter.cs
+++ Assets/Dany/Assets/Scripts/CoinCounter.cs
@@ -1,21 +1,30 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CoinCounter : MonoBehaviour
 {
     [SerializeField] int coins;
     [SerializeField] int coinValue;
+    [SerializeField] TextMeshProUGUI coinsText;
+
+    private void Update()
+    {
+        coinsText.text = "Coins: " + Mathf.RoundToInt(coins).ToString();
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Coin"))
         {
             coins += coinValue;
             //destruir monedas 
             //Destroy(collision.gameObject);
-            Destroy(this.gameObject);
+            collision.gameObject.SetActive(false);
+            
         }
     }
 }
